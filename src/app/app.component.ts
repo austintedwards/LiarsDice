2138e0cf0fcb8ae9fd01a8c5fa75faf2217521e2
Austin Edwards <austintedwards@gmail.com>
2017-04-10 21:53:15 -0600

    dice gifs added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { TabsPage } from '../pages/tabs/tabs';
+// import { DiceRollPage } from '../pages/dice-roll/dice-roll';
 
 
 @Component({
